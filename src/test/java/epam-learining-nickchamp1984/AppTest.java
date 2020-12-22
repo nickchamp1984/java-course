03323e4cbabaa835b7680530cc98949e497d6ee6
nickchamp1984 <47222866+nickchamp1984@users.noreply.github.com>
2020-12-23 01:05:30 +0200

    Initial EPAM Java course commit

---
+++ src/test/java/epam-learining-nickchamp1984/AppTest.java
@@ -0,0 +1,20 @@
+package epam-learining-nickchamp1984;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
